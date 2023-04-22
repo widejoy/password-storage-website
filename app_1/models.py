7bae155d17a35cf032261246bc200dd233639b25
widejoy <rogerantony40@ug.cusat.ac.in>
2023-04-22 15:28:59 +0530

    cerated database

--- app_1/models.py
+++ app_1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class login(models.Model):
+    email_id= models.IntegerField()
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)

dcede1d58940485db4c6c4cdd1c888298d51d998
widejoy <rogerantony40@ug.cusat.ac.in>
2023-04-22 15:31:29 +0530

    rend

--- app_1/models.py
+++ app_1/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class login(models.Model):
-    email_id= models.IntegerField()
+    email_id= models.CharField(max_length=50)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)

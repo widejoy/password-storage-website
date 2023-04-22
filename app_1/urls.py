25ccbb9a5bae4ae0365ad224c7a51339dd692283
widejoy <rogerantony40@ug.cusat.ac.in>
2023-04-22 19:42:23 +0530

    can now sign up

--- app_1/urls.py
+++ app_1/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [ 
     path('',views.home,name="home"),
-    path('about/',views.about,name="about")
+    path('signup/',views.signup,name="signup"),
+    path('signin/',views.signin,name="signin"),
+    path('signout/',views.signout,name="signout")
+
 ]
--- app_1/views.py
+++ app_1/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 def home(request):
-    return render(request,'home.html')
-def about(request):
-    return render(request,'about.html')
+    return render(request,'main.html')
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password_1 = request.POST.get('password1')
+        password_2 = request.POST.get('password2')
+        myuser = User.objects.create_user(username,email,password_1)
+        myuser.fname = first_name
+        myuser.lname = last_name
+        myuser.save() 
+        messages.success(request, "Account created")
+        return redirect('signin')
+
+    return render(request,'signup.html')
+def signin(request): 
+    return render(request,'signin.html')
+def signout(request):
+    pass

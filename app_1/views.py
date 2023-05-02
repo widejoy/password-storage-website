0e6155e32a581348e44e0157ea71bce48ad0f31f
widejoy <rogerantony40@ug.cusat.ac.in>
2023-05-02 22:51:02 +0530

    updates

--- app_1/views.py
+++ app_1/views.py
@@ -13,12 +13,18 @@ def signup(request):
         last_name = request.POST.get('last_name')
         password_1 = request.POST.get('password1')
         password_2 = request.POST.get('password2')
-        myuser = User.objects.create_user(username,email,password_1)
-        myuser.fname = first_name
-        myuser.lname = last_name
-        myuser.save() 
-        messages.success(request, "Account created")
-        return redirect('signin')
+        if password_1==password_2:
+         myuser = User.objects.create_user(username,email,password_1)
+         myuser.fname = first_name
+         myuser.lname = last_name
+         myuser.save() 
+         messages.success(request, "Account created")
+         return render(request,"signin.html")
+        if User.objects.filter(username = username).first():
+         messages.error(request, "This username is already taken")
+        else:
+            messages.error(request, "password is not equal")
+            
 
     return render(request,'signup.html')
 def signin(request):
